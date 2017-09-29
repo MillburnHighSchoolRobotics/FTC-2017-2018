607f71c11ffda0a8f8d86a13f717b33607b1324c
David Shustin <david.shustin@gmail.com>
2017-09-29 17:13:02 -0400

    Add Translation and Rotation

--- TeamCode/src/main/java/virtualRobot/logicThreads/competition/TeleOpCustomLogicRewrite.java
+++ TeamCode/src/main/java/virtualRobot/logicThreads/competition/TeleOpCustomLogicRewrite.java
@@ -2,6 +2,7 @@ package virtualRobot.logicThreads.competition;
 
 import virtualRobot.JoystickController;
 import virtualRobot.LogicThread;
+import virtualRobot.utils.MathUtils;
 
 /**
  * Created by david on 9/29/17.
@@ -25,9 +26,57 @@ public class TeleOpCustomLogicRewrite extends LogicThread {
                 {-1, 1, 1, -1},
                 {0, 1, 1, 0}
         };
+
         while (true) {
             controller1.logicalRefresh();
             controller2.logicalRefresh();
+            double translateTheta = Math.toDegrees(controller1.getValue(JoystickController.THETA_1));
+            double translateMag = controller1.getValue(JoystickController.R_1);
+            double rotateX = controller1.getValue(JoystickController.X_2);
+            if (translateTheta < 0) translateTheta += 360;
+            double scale;
+            double RF = 0, RB = 0, LF = 0, LB = 0;
+            if (!MathUtils.equals(rotateX, 0, 0.0001)) {
+                robot.getRFMotor().setPower(-rotateX);
+                robot.getRBMotor().setPower(-rotateX);
+                robot.getLFMotor().setPower(rotateX);
+                robot.getLBMotor().setPower(rotateX);
+            } else if (!MathUtils.equals(translateMag, 0, 0.0001)) {
+                double translatePower = translateMag * 0.66; //set later
+                if (MathUtils.equals(translateTheta, 90)) {
+                    //Forward
+                    RF = translatePower * POWER_MATRIX[0][0];
+                    RB = translatePower * POWER_MATRIX[0][1];
+                    LF = translatePower * POWER_MATRIX[0][2];
+                    LB = translatePower * POWER_MATRIX[0][3];
+                } else if (MathUtils.equals(translateTheta, 180)) {
+                    //Left
+                    RF = translatePower * POWER_MATRIX[2][0];
+                    RB = translatePower * POWER_MATRIX[2][1];
+                    LF = -translatePower * POWER_MATRIX[2][2];
+                    LB = -translatePower * POWER_MATRIX[2][3];
+                } else if (MathUtils.equals(translateTheta, 270)) {
+                    //Backward
+                    RF = translatePower * POWER_MATRIX[4][0];
+                    RB = translatePower * POWER_MATRIX[4][1];
+                    LF = translatePower * POWER_MATRIX[4][2];
+                    LB = translatePower * POWER_MATRIX[4][3];
+                } else if (MathUtils.equals(translateTheta, 0)) {
+                    //Right
+                    RF = translatePower * POWER_MATRIX[6][0];
+                    RB = translatePower * POWER_MATRIX[6][1];
+                    LF = -translatePower * POWER_MATRIX[6][2];
+                    LB = -translatePower * POWER_MATRIX[6][3];
+                }
+                robot.addToTelemetry("0", LF + " " + RF);
+                robot.addToTelemetry("1", LB + " " + RB);
+                robot.getRFMotor().setPower(RF);
+                robot.getRBMotor().setPower(RB);
+                robot.getLFMotor().setPower(LF);
+                robot.getLBMotor().setPower(LB);
+            } else {
+                robot.stopMotors();
+            }
             if (Thread.currentThread().isInterrupted())
                 throw new InterruptedException();
             Thread.sleep(10);
