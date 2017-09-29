8c1b47c3ea5b8994a12d698800549e3ceb8ce502
David Shustin <david.shustin@gmail.com>
2017-09-29 15:31:06 -0400

    Add Joystick test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingUpdate/JoystickUpdate.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.TestingUpdate;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.UpdateThread;
+
+import virtualRobot.logicThreads.testing.JoystickTelemetryLogic;
+
+/**
+ * Created by david on 9/29/17.
+ */
+
+@TeleOp( name = "Testing: Joystick", group = "Testing")
+public class JoystickUpdate extends UpdateThread {
+    @Override
+    public void setLogicThread() {
+        logicThread = JoystickTelemetryLogic.class;
+    }
+}
