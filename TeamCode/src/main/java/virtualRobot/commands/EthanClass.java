be12c65d0a8686d1c4e3291dfa9f4dafc144e2ea
David Shustin <david.shustin@gmail.com>
2017-11-17 11:03:52 -0500

    Fix EthanClass

--- TeamCode/src/main/java/virtualRobot/commands/EthanClass.java
+++ TeamCode/src/main/java/virtualRobot/commands/EthanClass.java
@@ -1,8 +1,11 @@
 package virtualRobot.commands;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 
 import org.firstinspires.ftc.teamcode.UpdateThread;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -22,6 +25,7 @@ import java.util.List;
 import virtualRobot.SallyJoeBot;
 import virtualRobot.VuforiaLocalizerImplSubclass;
 import virtualRobot.logicThreads.AutonomousLogicThread;
+import virtualRobot.utils.BetterLog;
 
 /**
  * Created by ethan on 9/22/17.
@@ -33,12 +37,18 @@ public class EthanClass extends Command {
 
     private int width, height;
     private int kSize = 11;
-    Scalar redUpper = new Scalar(50, 50, 200);
-    Scalar redLower = new Scalar(0, 0, 50);
-    Scalar blueUpper = new Scalar(50, 0, 0);
-    Scalar blueLower = new Scalar(200, 50, 50);
+    Scalar redUpper = new Scalar(255, 100, 100, 255);
+    Scalar redLower = new Scalar(50, 0, 0, 0);
+    Scalar blueUpper = new Scalar(50, 150, 255, 255);
+    Scalar blueLower = new Scalar(0, 0, 50, 0);
     private static final SallyJoeBot robot = Command.ROBOT;
-
+    static {
+        if(!OpenCVLoader.initDebug()){
+            BetterLog.d("OpenCV", "OpenCV not loaded");
+        } else {
+            BetterLog.d("OpenCV", "OpenCV loaded");
+        }
+    }
     public EthanClass() {
         vuforiaInstance = UpdateThread.vuforiaInstance;
         width = vuforiaInstance.rgb.getBufferWidth();
@@ -55,15 +65,19 @@ public class EthanClass extends Command {
         else
             throw new RuntimeException("Was not called in Autonomous?");
 //        Log.d("EthanClass", Integer.toString(point++));
-        Mat img = new Mat(height, width, CvType.CV_8UC1);
-        img.put(0,0,vuforiaInstance.rgb.getPixels().array());
-        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGR);
+        Mat img = new Mat();
+        Bitmap bm = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        bm.copyPixelsFromBuffer(vuforiaInstance.rgb.getPixels());
+        Utils.bitmapToMat(bm, img);
+        Mat imgbgr = new Mat();
+        Imgproc.cvtColor(img, imgbgr, Imgproc.COLOR_RGB2BGR);
 //        Log.d("EthanClass", Integer.toString(point++));
         try {
-            robot.sendCVTelemetry("Image", img).execute();
+            robot.sendCVTelemetry("Image", imgbgr).execute();
         } catch (IOException e) {
             robot.addToTelemetry("Image", e.getMessage());
         }
+        imgbgr.release();
 //        Log.d("EthanClass", Integer.toString(point++));
         Mat blur = new Mat();
         Imgproc.GaussianBlur(img, blur, new Size(kSize,kSize), 0);
@@ -96,7 +110,7 @@ public class EthanClass extends Command {
         Core.inRange(img, blueLower, blueUpper, blue);
 //        Log.d("EthanClass", Integer.toString(point++));
         try {
-            robot.sendCVTelemetry("Blue", img).execute();
+            robot.sendCVTelemetry("Blue", blue).execute();
         } catch (IOException e) {
             robot.addToTelemetry("BlueImage", e.getMessage());
         }
