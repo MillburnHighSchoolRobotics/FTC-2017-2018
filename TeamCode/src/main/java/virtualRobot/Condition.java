26f58c9fa4f3e8a113dbfe2c19944d56bb3f64c1
Ethan Mak <ethan.mak100@gmail.com>
2018-01-05 12:22:08 -0500

    Update to 3.6

--- TeamCode/src/main/java/virtualRobot/Condition.java
+++ TeamCode/src/main/java/virtualRobot/Condition.java
@@ -3,7 +3,7 @@ package virtualRobot;
 /**
  * Created by ethachu19 on 3/31/17
  *
- * Conditional thtat is bound to a certain action in LogicThread
+ * Conditional that is bound to a certain action in LogicThread
  */
 public interface Condition {
     boolean isConditionMet();
