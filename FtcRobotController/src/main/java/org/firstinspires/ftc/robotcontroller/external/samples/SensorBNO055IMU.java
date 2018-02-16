c7a4e5cd1653b002429c0ea4082ffead48fbff67
David Shustin <david.shustin@gmail.com>
2018-02-16 14:39:52 -0500

    Add IMU Sensor test

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorBNO055IMU.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorBNO055IMU.java
@@ -55,7 +55,7 @@ import java.util.Locale;
  * @see <a href="http://www.adafruit.com/products/2472">Adafruit IMU</a>
  */
 @Autonomous(name = "Sensor: BNO055 IMU", group = "Sensor")
-@Disabled                            // Comment this out to add to the opmode list
+//@Disabled                            // Comment this out to add to the opmode list
 public class SensorBNO055IMU extends LinearOpMode
     {
     //----------------------------------------------------------------------------------------------
@@ -168,6 +168,7 @@ public class SensorBNO055IMU extends LinearOpMode
                                     + gravity.zAccel*gravity.zAccel));
                     }
                 });
+        telemetry.addLine().addData("status", imu.getSystemStatus() + " " + imu.getCalibrationStatus());
     }
 
     //----------------------------------------------------------------------------------------------
